4da0c509cdb5ec5cc2d2ca5a69c7fcc48943d9f3
David West <dwest1507@gmail.com>
2020-09-05 15:34:34 -0400

    Changed from read_csv to read.csv. That way strings are automatically loaded as factors. This helps for piping this data through ML algos.

--- src/data/make_dataset.R
+++ src/data/make_dataset.R
@@ -1,4 +1,4 @@
 # Loading the data
-train <- read_csv(here::here("data","raw","train.csv"))
-test <- read_csv(here::here("data","raw","test.csv"))
-submission_example <- read_csv(here::here("data","raw","gender_submission.csv"))
+train <- read.csv(here::here("data","raw","train.csv"))
+test <- read.csv(here::here("data","raw","test.csv"))
+submission_example <- read.csv(here::here("data","raw","gender_submission.csv"))
