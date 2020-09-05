88e92b75df2b84272c018442a8844e015c0e4f36
David West <dwest1507@gmail.com>
2020-09-05 15:56:47 -0400

    The Survived column is now a factor instead of an integer. This will make it easier to pipe through ML algos.

--- src/data/make_dataset.R
+++ src/data/make_dataset.R
@@ -2,3 +2,7 @@
 train <- read.csv(here::here("data","raw","train.csv"))
 test <- read.csv(here::here("data","raw","test.csv"))
 submission_example <- read.csv(here::here("data","raw","gender_submission.csv"))
+
+# Changing Survived from an integer to a factor for ML models
+train$Survived <- factor(train$Survived)
+test$Survived <- factor(test$Survived)
