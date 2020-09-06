7de2a6b6c0f7cf7480d202cbc68ffed444e61531
David West <dwest1507@gmail.com>
2020-09-06 15:22:13 -0400

    No need to convert the column "Survived" into a factor, when it doesn't exist in the data set. Dummy!

--- src/data/make_dataset.R
+++ src/data/make_dataset.R
@@ -5,4 +5,3 @@ submission_example <- read.csv(here::here("data","raw","gender_submission.csv"))
 
 # Changing Survived from an integer to a factor for ML models
 train$Survived <- factor(train$Survived)
-test$Survived <- factor(test$Survived)
